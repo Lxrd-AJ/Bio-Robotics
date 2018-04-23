e65a224a67faf26edf283149aca2d903ddd302a4
Ibraheem AJ <ibraheemaj@icloud.com>
2018-04-23 22:56:54 +0100

    Charts in progress

--- Server/webapp/flower/src/app/app.module.ts
+++ Server/webapp/flower/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { FlowerService } from './flower.service';
 import { FlowersComponent } from './flowers/flowers.component';
 import { FlowerComponent } from './flower/flower.component';
+import { Browser } from 'protractor';
 
 
 @NgModule({
@@ -23,7 +26,9 @@ import { FlowerComponent } from './flower/flower.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgxChartsModule
   ],
   providers: [FlowerService],
   bootstrap: [AppComponent]
--- Server/webapp/flower/src/app/flower/flower.component.ts
+++ Server/webapp/flower/src/app/flower/flower.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FlowerService } from './../flower.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-flower',
@@ -10,13 +11,35 @@ import { Location } from '@angular/common';
 })
 export class FlowerComponent implements OnInit {
 
-  flower:Object
+  flower:Object;
+  plot_data: any [];
+  view: any[] = [700, 400];
 
   constructor(private flowerService: FlowerService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
-    this.flowerService.getFlower(id).subscribe(res => {this.flower = res[0]; console.info(res[0])});
+    this.flowerService.getFlower(id).subscribe(res => {
+      this.flower = res[0]; 
+      this.setupPlotData( this.flower );
+      console.info(res[0])
+    });
+  }
+
+  setupPlotData( flower ){
+    this.plot_data = [{
+      "name": "Humidity",
+      "series": [
+        {
+          "name": "2010",
+          "value": 7870000
+        },
+        {
+          "name": "2011",
+          "value": 8270000
+        }
+      ]
+    }]
   }
 
 }
